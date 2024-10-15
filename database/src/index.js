a09a1259cb14fa03090280f505cf462c54274469
Seth <8257011+UwUhq@users.noreply.github.com>
2024-10-15 23:05:09 +0200

    Update index.js

--- database/src/index.js
+++ database/src/index.js
@@ -32,91 +32,93 @@ class DataBase {
         const options = { table: tableName || params.ops.table || "json" };
         this.db.prepare(`CREATE TABLE IF NOT EXISTS ${options.table} (ID TEXT, json TEXT)`).run();
         if (params.ops.target && params.ops.target[0] === ".") params.ops.target = params.ops.target.slice(1);
-        if (params.data && params.data === Infinity) return this.error(`Vous ne pouvez pas mettre Infinity dans la base de données {ID: ${params.id}}`);
+        if (params.data && params.data === Infinity) return this.error(`You cannot put Infinity in the database {ID: ${params.id}}`);
         if (params.stringify) {
             try {
-                params.data = JSON.stringify(params.data)
+                params.data = JSON.stringify(params.data);
             } catch (e) {
-                this.error(`Veuillez fournir une entrée valide {ID: ${params.id}}\nErreur Message: ${e.message}`)
+                this.error(`Please provide a valid input {ID: ${params.id}}\nError Message: ${e.message}`);
             }
         };
         if (params.id && params.id.includes(".")) {
             const unparsed = params.id.split(".");
             params.id = unparsed.shift();
-            params.ops.target = unparsed.join(".")
+            params.ops.target = unparsed.join(".");
         };
         return this.methods[method](this.db, params, options);
     }
 
     fetch(key, ops) {
-        if (!key) this.error("Aucune clé spécifiée");
-        return this.go("fetch", { id: key, ops: ops || {} })
+        if (!key) this.error("No key specified");
+        return this.go("fetch", { id: key, ops: ops || {} });
     };
+
     get(key, ops) {
-        if (!key) this.error("Aucune clé spécifiée");
-        return this.go("fetch", { id: key, ops: ops || {} })
+        if (!key) this.error("No key specified");
+        return this.go("fetch", { id: key, ops: ops || {} });
     };
 
     set(key, value, ops) {
-        if (!key) this.error("Aucune clé spécifiée");
-        if (!value) this.error("Aucune valeur spécifiée");
+        if (!key) this.error("No key specified");
+        if (!value) this.error("No value specified");
         return this.go("set", {
             stringify: true,
             id: key,
             data: value,
             ops: ops || {}
-        })
+        });
     };
 
     add(key, value, ops) {
-        if (!key) this.error("Aucune clé spécifiée");
-        if (isNaN(value)) this.error(`Merci de spécifier le montant à ajouter {ID: ${key}}`);
-        return this.go("add", { id: key, data: value, ops: ops || {} })
+        if (!key) this.error("No key specified");
+        if (isNaN(value)) this.error(`Please specify the amount to add {ID: ${key}}`);
+        return this.go("add", { id: key, data: value, ops: ops || {} });
     };
 
     subtract(key, value, ops) {
-        if (!key) this.error("Aucune clé spécifiée");
-        if (isNaN(value)) this.error(`Merci de préciser le montant à soustraire {ID: ${key}}`);
-        return this.go("subtract", { id: key, data: value, ops: ops || {} })
+        if (!key) this.error("No key specified");
+        if (isNaN(value)) this.error(`Please specify the amount to subtract {ID: ${key}}`);
+        return this.go("subtract", { id: key, data: value, ops: ops || {} });
     };
 
     push(key, value, ops) {
-        if (!key) this.error("Aucune clé spécifiée");
-        if (!value && value != 0) this.error(`Merci de spécifier la valeur à ajouter au tableau {ID: ${key}}`);
+        if (!key) this.error("No key specified");
+        if (!value && value != 0) this.error(`Please specify the value to add to the array {ID: ${key}}`);
         return this.go("push", {
             stringify: true,
             id: key,
             data: value,
             ops: ops || {}
-        })
+        });
     };
 
     delete(key, ops) {
-        if (!key) this.error("Aucune clé spécifiée");
-        return this.go("delete", { id: key, ops: ops || {} })
+        if (!key) this.error("No key specified");
+        return this.go("delete", { id: key, ops: ops || {} });
     };
 
     has(key, ops) {
-        if (!key) this.error("Aucune clé spécifiée");
-        return this.go("has", { id: key, ops: ops || {} })
+        if (!key) this.error("No key specified");
+        return this.go("has", { id: key, ops: ops || {} });
     };
 
     includes(key, ops) {
-        if (!key) this.error("Aucune clé spécifiée");
-        return this.go("has", { id: key, ops: ops || {} })
+        if (!key) this.error("No key specified");
+        return this.go("has", { id: key, ops: ops || {} });
     };
 
     all(ops) {
-        return this.go("all", { ops: ops || {} })
+        return this.go("all", { ops: ops || {} });
     };
 
     fetchAll(ops) {
-        return this.go("all", { ops: ops || {} })
+        return this.go("all", { ops: ops || {} });
     };
 
     type(key, ops) {
-        if (!key) this.error("Aucune clé spécifiée");
-        return this.go("type", { id: key, ops: ops || {} })
+        if (!key) this.error("No key specified");
+        return this.go("type", { id: key, ops: ops || {} });
     };
 };
+
 module.exports = DataBase;
