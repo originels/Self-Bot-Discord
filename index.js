2f91ac79aa95ea46cf021ab30b588deeb5c29cb2
Seth <8257011+UwUhq@users.noreply.github.com>
2024-10-15 22:29:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const DataBase = require("./database/src/index.js");
+const db = new DataBase("./database/data/data.sqlite");
+
+const Client = require("./utils/client.js");
+const client = new Client();
+
+const handler = require("./utils/handler.js");
+const config = require("./utils/config.json");
+
+const { logError, consoleError } = require('./structures/gestion/error.js');
+handler(client, db, config).then(() => client.go());
+
+process.on("unhandledRejection", err => {
+
+    logError(err, client, db, config)
+    consoleError(err, client, db, config);
+    if (err.message) return;
+});
+process.on('uncaughtExceptionMonitor', err => {
+
+    logError(err, client, db, config)
+    consoleError(err, client, db, config);
+    if (err.message) return;
+});
+process.on("unhandledRejection", err => {
+
+    logError(err, client, db, config)
+    consoleError(err, client, db, config);
+    if (err.message) return;
+});
